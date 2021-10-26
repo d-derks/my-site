e3ad54dbc88a4419fe1ede2ed27e69eecc6fcf50
debbie.derks <debbie.derks@endava.com>
2021-10-26 08:54:28 +0200

    [FEATURE] BaseLayout: add header/footer placeholder

--- src/BaseLayout.js
+++ src/BaseLayout.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles/page.module.scss';
 
 function BaseLayout({children}) {
     return (
-        <main className="main">
-            {children}
-        </main>
+       <>
+           <header style={{position: 'fixed', top:'0', backgroundColor: '#f1f1f1', width: '100%'}}>
+               <div style={{minHeight: '8rem', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                   <h2 style={{margin: 'auto'}}>
+                       Header
+                   </h2>
+               </div>
+           </header>
+           <main className={styles.main}>
+               {children}
+           </main>
+           <footer style={{minHeight: '100vh'}}>
+               <div style={{minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                   <h2 style={{margin: 'auto'}}>
+                       Footer
+                   </h2>
+               </div>
+           </footer>
+       </>
     );
 }
 
